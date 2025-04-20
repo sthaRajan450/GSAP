354d668a07e2af168bc82ce3dd3eb3c5fe1ec299
rajan shrestha <stharajan477@gmail.com>
2025-04-20 17:26:42 +0545

    svg animation

--- script.js
+++ script.js
@@ -32,16 +32,13 @@
 //     yoyo:true
 // })
 
-
-
-
 // timeline
 // let tl = gsap.timeline();
 // tl.from("#nav h1", {
 //   opacity: 0,
 //   y: -30,
 //   duration: 1,
-  
+
 // });
 // tl.from(".part2 h2", {
 //   opacity: 0,
@@ -54,10 +51,8 @@
 //     opacity:0,
 //     duration:1,
 //     scale:0.5,
-   
+
 // })
-
-
 
 //scrollTrigger
 // gsap.from('#page2 #box',{
@@ -71,7 +66,7 @@
 //         scroller:'body',
 //         markers:true,
 //         start:'top 60%',
-//         end:'top 30%', 
+//         end:'top 30%',
 //         scrub:2
 //     }
 // })
@@ -80,7 +75,7 @@
 //     opacity:0,
 //     x:500,
 //     duration:2,
-//     delay:1,  
+//     delay:1,
 //     scrollTrigger:{
 //         trigger:'#page1 h1',
 //         duration:2,
@@ -99,19 +94,37 @@
 //         markers:true,
 //         start:'top 30%'
 //     }
- 
+
 // })
 
+// //pin
+// gsap.to("#page2 h1", {
+//   transform: "translateX(-50%)",
+//   scrollTrigger: {
+//     trigger: "#page2",
+//     start: "top 0%",
+//     end: "top -150%",
+//     markers: true,
+//     scrub: 2,
+// pin: true,
+//   },
+// });
 
-//pin
-gsap.to("#page2 h1",{
-    transform:'translateX(-50%)',
-    scrollTrigger:{
-        trigger:"#page2",
-        start:'top 0%',
-        end:'top -150%',
-        markers:true,
-        scrub:2,
-        pin:true
-    }
-})
+let initialPath = `M 10 200 Q 500 200 990 200`;
+let finalPath = `M 10 200 Q 500 200 990 200`;
+
+let curve = document.querySelector("#curve");
+curve.addEventListener("mousemove", (e) => {
+  initialPath = `M 10 200 Q ${e.x} ${e.y} 990 200`;
+  gsap.to("svg path", {
+    attr: { d: initialPath },
+    ease:"power3.out"
+  });
+});
+curve.addEventListener("mouseleave", (e) => {
+  gsap.to('svg path',{
+    attr:{d:finalPath},
+    duration:2,
+    ease:"elastic.out(1.2,0.1)"
+  })
+});
